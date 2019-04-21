8daa52e621615061f57de76786c853cde1c63f9b
luxumbra <hello@crafted.im>
2019-04-21 09:29:46 +0100

    better config?

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -2,14 +2,16 @@ const {
   api: { projectId, dataset }
 } = require('../studio/sanity.json')
 
-require('dotenv').config()
+let env = process.env.NODE_ENV || 'development'
+
+require('dotenv').config({ path: `./.env.${env}` })
 
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-plugin-rollbar',
       options: {
-        accessToken: process.env.ROLLBAR_SECRET_TOKEN,
+        accessToken: `${process.env.ROLLBAR_SECRET_TOKEN}`,
         // For all configuration options, see https://docs.rollbar.com/v1.0.0/docs/rollbarjs-configuration-reference
         captureUncaught: true,
         captureUnhandledRejections: true,
@@ -35,9 +37,9 @@ module.exports = {
         dataset,
         // To enable preview of drafts, copy .env-example into .env,
         // and add a token with read permissions
-        token: process.env.SANITY_TOKEN,
-        watchMode: process.env.SANITY_WATCHMODE,
-        overlayDrafts: process.env.SANTIY_OVERLAY_DRAFTS
+        token: `${process.env.SANITY_TOKEN}`,
+        watchMode: `${process.env.SANITY_WATCHMODE}`,
+        overlayDrafts: `${process.env.SANTIY_OVERLAY_DRAFTS}`
       }
     },
     `gatsby-plugin-sharp`,
