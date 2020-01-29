cf2887afad62648b32babd0075bc1cd354990d75
luxumbra <hello@crafted.im>
2020-01-29 17:54:55 +0000

    Adding Netlify widget to the dashboard

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,24 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Sanity Studio",
+            apiId: "xxxxx-yyyy-zzzz-xxxx-yyyyyyyy",
+            buildHookId: "xxxyyyxxxyyyyxxxyyy",
+            name: "sanity-gatsby-blog-20-studio"
+          },
+          {
+            title: "Website",
+            apiId: "yyyyy-xxxxx-zzzz-xxxx-yyyyyyyy",
+            buildHookId: "yyyyxxxxxyyyxxdxxx",
+            name: "sanity-gatsby-blog-20-web"
+          }
+        ]
+      }
+    }
+  ]
+};
